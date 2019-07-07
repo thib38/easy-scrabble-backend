c5d4e19f66661b13518349b95f12d0424e8548a9
thib38 <thibault.enipel@gmail.com>
2019-07-07 17:45:40 +0200

    add log file to waitress with log rotation in /var/log/scrabble

--- waitress_launch.py
+++ waitress_launch.py
@@ -1,8 +1,24 @@
 from waitress import serve
 import scrabble
-import hug
+import sys
 import logging
+import datetime
+from logging.handlers import RotatingFileHandler
+
+# check log_file name validity
+log_file_name = '/var/log/scrabble/' + datetime.datetime.today().strftime('%Y%m%d_%H%M%S_') + 'waitress.log'
+try:
+    with open(log_file_name, 'x') as tempfile:  # OSError if file exists or is invalid
+        pass
+except OSError as e:
+    print("Invalid --log_file name: %s - %s" % (log_file_name, e))
+    sys.exit(-1)
+
 logger = logging.getLogger('waitress')
 logger.setLevel(logging.DEBUG)
+
+handler_file = RotatingFileHandler(log_file_name, maxBytes=100000000, backupCount=5)
+handler_file.setLevel(logging.DEBUG)
+logger.addHandler(handler_file)
 
 serve(scrabble.__hug_wsgi__, listen='127.0.0.1:8000', threads=128)
